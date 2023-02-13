c13704bce786127612aecfefb34881f2b2889e4b
Antonio <agilardi@pslogin02.slac.stanford.edu>
2023-02-13 13:10:53 -0800

    Adding an author test

---
+++ project_name/tests/test_something.py
@@ -0,0 +1,8 @@
+def test_author(author_name):
+    
+    if (author_name=="Antonio"):
+        print("Antonio is the author")
+    else:
+        print("Unknown author")
+
+    assert author_name == Antonio
